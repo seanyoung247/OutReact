148f04c827d61f2d7aae5cdd398a61b05a2b0428
sean <seanyoung247@gmail.com>
2024-10-20 22:30:38 +0100

    feat: Adds road

---
+++ frontend-src/src/components/road3d/buildroad.tsx
@@ -0,0 +1,35 @@
+import { RoadSegment } from './roadsegment'
+import { RoadSegmentDescriptor } from '../../testRoad'
+import { wrap } from '../../utilities/misc'
+
+
+export const buildRoad = (road: RoadSegmentDescriptor[], first: number, length: number) => {
+    const segments = [
+        <RoadSegment key={first - 1} z={first - 1} topX={0} baseX={0} 
+            descriptor={road[wrap(0, first - 1, road.length)]} />
+    ]
+    let z = first
+    let topX = 0
+    let baseX = 0
+
+    /* Builds a segment from one or more segment descriptors (allows LOD) */
+    const buildSegment = (lod:number = 1) => {
+        const segment = (
+            <RoadSegment key={z} 
+                z={z} 
+                baseX={baseX} 
+                topX={(baseX += topX)} 
+                descriptor={road[wrap(0, z, road.length)]}
+            />
+        )
+        topX += road[wrap(0, z, road.length)].curve
+        z += lod
+        return segment
+    }
+
+    for (let i = 0; i < length; i++) {
+        segments.push(buildSegment())
+    }
+
+    return segments
+}
---
+++ frontend-src/src/components/road3d/roadsegment.tsx
@@ -0,0 +1,20 @@
+
+import { classList } from '../../utilities/css'
+import { RoadSegmentDescriptor } from '../../testRoad'
+
+import './roadsegment.css'
+
+type Props = {
+    z: number,  // Z Position
+    topX: number, // Top X offset
+    baseX: number, // Bottom X offset
+    descriptor: RoadSegmentDescriptor,
+}
+
+export const RoadSegment = ({z, topX, baseX, descriptor}:Props) => (
+    <div className={classList(
+            'road-segment',
+            descriptor?.classes
+        )} 
+        style={{'--z': z, '--tX': topX, '--bX': baseX}}/>
+)
