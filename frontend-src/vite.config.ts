8cf24814acd4ea51eb107a1925bce8658494f748
sean <seanyoung247@gmail.com>
2024-10-20 18:19:43 +0100

    chore: Adds path aliases

--- frontend-src/vite.config.ts
+++ frontend-src/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '~', replacement: path.resolve(__dirname, 'src') },
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') }
+    ],
+  },
   build: { outDir: '../frontend' },
   plugins: [react()],
 })
