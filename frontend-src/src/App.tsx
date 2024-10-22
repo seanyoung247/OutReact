49db6fd92e7f70a42a315c6e83dcb3c275805ec2
sean <seanyoung247@gmail.com>
2024-10-22 22:29:53 +0100

    feat: basic background panning

--- frontend-src/src/App.tsx
+++ frontend-src/src/App.tsx
@@ -6,8 +6,10 @@ import { Road } from './components/road3d/road'
 import { useScrollState } from './hooks/scroll'
 // import { useState } from 'react'
 // import { useAnimationFrame } from './hooks/frame'
+import { useRef } from 'react'
 
 import { roadSegments } from './testRoad'
+import { wrap } from './utilities/misc'
 
 import './App.css'
 
@@ -21,17 +23,27 @@ function App() {
     //     setCamZ(camZ + (0.008 * time))
     // })
 
+    const baseSeg = Math.max(Math.floor(camZ), 0)
+    const xPos = useRef({camX:0, baseSeg})
+
+    if (xPos.current.baseSeg !== baseSeg) {
+        xPos.current.camX += (
+            roadSegments[wrap(0, baseSeg, roadSegments.length)].curve *
+            Math.sign(scroll.delta.y)
+        )
+    }
+
     return (
         <>
             <div className='scroller' 
                 style={{
-                    '--scroll-height': '5000px'
+                    '--scroll-height': '10000px'
                 }}
             />
 
-            <Background horizon={35}>
+            <Background horizon={35} xPos={xPos.current.camX}>
                 <View3D width='100%' height='100lvh' perspective={4} horizon={35}>
-                    <Road road={roadSegments} camZ={camZ}/>
+                    <Road road={roadSegments} length={20} camZ={camZ}/>
                 </View3D>
             </Background>
         </>
--- frontend-src/src/components/background.tsx
+++ frontend-src/src/components/background.tsx
@@ -1,15 +1,21 @@
 
+import Img from '../assets/testbackground.png'
 import './background.css'
 
 type Props = {
     horizon: number,
+    xPos: number,
     children?: React.ReactNode
 }
 
-export const Background = ({horizon, children}:Props) => (
+export const Background = ({horizon, xPos, children}:Props) => (
     <div
         className="background"
-        style={{'--h': horizon}}
+        style={{
+            '--h': horizon,
+            '--x': `${xPos/100}%`,
+            '--img': `url(${Img})`,
+        }}
     >
         { children }
     </div>
