3e667d7aa644802bf8e63933dcb89846d6245d40
sean <seanyoung247@gmail.com>
2024-10-27 15:47:17 +0000

    feat: Road positioning math

--- frontend-src/src/components/road3d/road.tsx
+++ frontend-src/src/components/road3d/road.tsx
@@ -1,6 +1,9 @@
 
 import { RoadSegmentDescriptor } from "../../testRoad"
 
+// import { RoadSegmentDescriptor, testSegments } from './utils'
+import { wrap } from '../../utilities/misc'
+
 import './road.css'
 import './road-texture.css'
 
@@ -10,14 +13,53 @@ type Props = {
     camZ: number,
 }
 
+const buildRoad = (start:number, length:number, road:RoadSegmentDescriptor[]) => {
+    const segments = []
+    let topX = 0, baseX = 0
+
+    for (let z = start; z < start + length; z++) {
+        segments.push(
+            <div key={z} className="road-segment"
+                style={{
+                    '--z': z,
+                    '--bX': baseX,
+                    '--tX': (baseX += topX),
+                    '--f': (topX += road[wrap(0, z, road.length)].curve),
+                }}
+            />
+        )
+    }
+
+    return segments
+}
+
 export const Road = ({road, length, camZ}:Props) => {
-    const segments = new Array(length).fill(0).map((_,i) => (
-        <div className="road-segment"
-            style={{ '--i': i, '--sc': 20}}
-        />
-    ))
+    // let topX = 0
+    // let baseX = 0
 
-    console.log(road, camZ)
+    // const segments = new Array(length).fill(0).map((_,i) => (
+    //     <div key={i} className="road-segment"
+    //         style={{
+    //             '--i': i,
+    //             '--bX': baseX,
+    //             '--tX': (baseX += topX),
+    //             '--f': (topX += testSegments[i].x),
+    //         }}
+    //     />
+    // ))
 
-    return <>{ segments }</>
+    // console.log(road, camZ)
+
+    const segments = buildRoad(Math.floor(camZ), length, road)
+
+    return (
+        <div className="road"
+            style={{
+                '--sc': 20,
+                '--camZ': camZ
+            }}
+        >
+            { segments }
+        </div>
+    )
 }
