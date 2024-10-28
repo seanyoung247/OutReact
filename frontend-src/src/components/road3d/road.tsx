6ac3e5ed3a4c79d51f5088157e210ddf43277a95
sean <seanyoung247@gmail.com>
2024-10-28 20:25:17 +0000

    refactor: Moves road building to seperate function

--- frontend-src/src/components/road3d/road.tsx
+++ frontend-src/src/components/road3d/road.tsx
@@ -1,62 +1,47 @@
 
-import { RoadSegmentDescriptor } from "../../testRoad"
+import {  RoadSegmentDescriptor } from "./utils"
 
-// import { RoadSegmentDescriptor, testSegments } from './utils'
 import { wrap } from '../../utilities/misc'
 
 import './road.css'
 import './road-texture.css'
 
 type Props = {
-    road: RoadSegmentDescriptor[],
+    road: RoadSegmentDescriptor,
     length: number, 
     camZ: number,
 }
 
-const buildRoad = (start:number, length:number, road:RoadSegmentDescriptor[]) => {
+const buildRoad = (start:number, length:number, road:RoadSegmentDescriptor) => {
     const segments = []
     let topX = 0, baseX = 0
 
     for (let z = start; z < start + length; z++) {
+        const segment = road[wrap(0, z, road.length)]
         segments.push(
             <div key={z} className="road-segment"
                 style={{
                     '--z': z,
                     '--bX': baseX,
                     '--tX': (baseX += topX),
-                    '--f': (topX += road[wrap(0, z, road.length)].curve),
                 }}
             />
         )
+        topX += segment.curve
     }
 
     return segments
 }
 
 export const Road = ({road, length, camZ}:Props) => {
-    // let topX = 0
-    // let baseX = 0
-
-    // const segments = new Array(length).fill(0).map((_,i) => (
-    //     <div key={i} className="road-segment"
-    //         style={{
-    //             '--i': i,
-    //             '--bX': baseX,
-    //             '--tX': (baseX += topX),
-    //             '--f': (topX += testSegments[i].x),
-    //         }}
-    //     />
-    // ))
-
-    // console.log(road, camZ)
-
     const segments = buildRoad(Math.floor(camZ), length, road)
+    // const baseSeg = road[wrap(0, Math.floor(camZ), road.length)]
 
     return (
         <div className="road"
             style={{
                 '--sc': 20,
-                '--camZ': camZ
+                '--camZ': camZ,
             }}
         >
             { segments }
