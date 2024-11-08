38622acb1aaef8270e54b1866fd9edd559744359
sean <seanyoung247@gmail.com>
2024-11-08 23:21:49 +0000

    refactor: Road texture simplified and improved

--- frontend-src/src/components/road3d/road.tsx
+++ frontend-src/src/components/road3d/road.tsx
@@ -4,14 +4,7 @@ import { RoadSegmentDescriptor } from "./utils"
 import { ScrollContext } from '../scrollmanager'
 
 import './road.css'
-// import './road-texture.css'
-import { wrap } from '../../utilities/misc'
-
-import roadTex from '../../assets/road.svg'
-import roadLip from '../../assets/road-lip.svg'
-
-// import roadTex from '../../assets/road.png'
-// import roadLip from '../../assets/road.png'
+import './road-texture.css'
 
 type Props = {
     road: RoadSegmentDescriptor,
@@ -20,41 +13,24 @@ type Props = {
 
 const Segment = ({style}:{style:CSSProperties}) => (
     <div className='road-segment' style={style}>
-        <svg className='road-lip'
-            viewBox='0 0 100 100' 
-            preserveAspectRatio='none'>
-            <g>
-                <rect className='road-tarmac-light'
-                    width="100%" height="100%"
-                    x="0" y="0"
-                />
-            </g>
-        </svg>
         <svg className='road-texture'
-            viewBox='0 0 100 100' 
+            viewBox='0 0 100 100'
             preserveAspectRatio='none'>
             <g>
                 <rect className='road-tarmac-light'
-                    width='100%' height='55%'
-                    x='0' y='0'
+                    x='0' y='0' width='100%' height='100%'
                 />
                 <rect className='road-tarmac-dark'
-                    width='100%' height='50%'
-                    x='0' y='50%'
+                    x='0' y='50%' width='100%' height='50%'
                 />
-            </g>
-            <g>
                 <rect className='road-lane-line'
-                    width='2%' height='50%'
-                    x='49%' y='25%'
+                    x='49%' y='25%' width='2%' height='50%'
                 />
                 <rect className='road-edge-line'
-                    width='2%' height='100%'
-                    x='2' y='0'
+                    x='2%' y='0' width='2%' height='100%'
                 />
                 <rect className='road-edge-line'
-                    width='2%' height='100%'
-                    x='96%' y='0'
+                    x='96%' y='0' width='2%' height='100%'
                 />
             </g>
         </svg>
@@ -66,15 +42,8 @@ const buildRoad = (start:number, length:number, road:RoadSegmentDescriptor) => {
     let topX = 0, baseX = 0
 
     for (let z = start; z < start + length; z++) {
-        const segment = road[wrap(0, z, road.length)]
+        const segment = road[z % road.length]
         segments.push(
-            // <div key={z - start} className="road-segment"
-            //     style={{
-            //         '--z': z,
-            //         '--bX': baseX,
-            //         '--tX': (baseX += topX),
-            //     }}
-            // />
             <Segment key={z - start}
                 style={{
                     '--z': z,
@@ -91,7 +60,7 @@ const buildRoad = (start:number, length:number, road:RoadSegmentDescriptor) => {
 
 export const Road = ({road, length}:Props) => {
     const {camZ} = useContext(ScrollContext)
-    const z = wrap(0, Math.floor(camZ), road.length)
+    const z = Math.floor(camZ) % road.length
     const segments = useMemo(
         () => buildRoad(z, length, road),
         [road, length, z]
@@ -102,11 +71,9 @@ export const Road = ({road, length}:Props) => {
             style={{
                 '--sc': length,
                 '--length': road.length,
-                '--camZ': wrap(0, camZ, road.length),
+                '--camZ': camZ % road.length,
                 '--z': z,
                 '--bC': road[z].curve,
-                '--tex': `url(${roadTex})`,
-                '--lip': `url(${roadLip})`
             }}
         >
             { segments }
