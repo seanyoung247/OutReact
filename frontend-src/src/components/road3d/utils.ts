007ab95046a1206200924915d75f73b6688f4cc8
sean <seanyoung247@gmail.com>
2024-11-12 19:55:41 +0000

    feat: adds initial road loading function

--- frontend-src/src/components/road3d/utils.ts
+++ frontend-src/src/components/road3d/utils.ts
@@ -3,8 +3,20 @@ export type RoadSegmentDescriptor = {
     curve: number,
 }[]
 
+export type RoadDescriptor = {
+    curve: number,
+    x: number,
+}[]
+
 export type RoadSegments = {
     z: number,
     bX: number,
     tX: number,
 }[]
+
+export const formatRoad = (road:RoadSegmentDescriptor):RoadDescriptor => {
+    let x = 0;
+    return road.map(v => ({
+        ...v, x: (x += v.curve)
+    }))
+}
