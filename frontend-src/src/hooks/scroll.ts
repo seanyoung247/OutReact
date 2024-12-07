7d783bedf7274508b023c0a5c94d8fb1b6b834df
sean <seanyoung247@gmail.com>
2024-12-07 15:47:52 +0000

    feat: Allows scroll hook to target elements

--- frontend-src/src/hooks/scroll.ts
+++ frontend-src/src/hooks/scroll.ts
@@ -20,6 +20,8 @@ export type ScrollData = {
 
 export type ScrollCallback = (s:ScrollData) => void
 
+type ScrollElement = HTMLElement | undefined | null
+
 //
 // Internal utilities
 //
@@ -38,12 +40,30 @@ const scrollStateChanged = (last:ScrollData, delta:Point):boolean => (
         Math.sign(last.delta.y) !== Math.sign(delta.y)
 )
 
-const getScrollData = (last: ScrollData): ScrollData => {
-    const current = { x: window.scrollX, y: window.scrollY }
+const scrollPosition = (el?: ScrollElement) => {
+    return (el ? 
+        {
+            x: el.scrollLeft,
+            y: el.scrollTop,
+            width: el.scrollWidth - el.clientWidth,
+            height: el.scrollHeight - el.clientHeight,
+        } : 
+        {
+            x: window.scrollX,
+            y: window.scrollY,
+            width: document.body.scrollWidth - window.innerWidth,
+            height: document.body.scrollHeight - window.innerHeight,
+        }
+    )
+}
+
+const getScrollData = (last: ScrollData, el?: ScrollElement): ScrollData => {
+    const scroll = scrollPosition(el)
+    const current = { x: scroll.x, y: scroll.y }
     const previous = last.current
     const progress = {
-        x: (current.x / (document.body.scrollWidth - window.innerWidth)) * 100,
-        y: (current.y / (document.body.scrollHeight - window.innerHeight)) * 100
+        x: (current.x / scroll.width) * 100,
+        y: (current.y / scroll.height) * 100
     }
     const delta = {
         x: current.x - previous.x,
@@ -63,25 +83,31 @@ const options = ({ passive: true } as unknown) as EventListenerOptions
 //
 // Hooks
 //
-/*
- * Hook that updates component with current scroll data
+/**
+ * Hook that updates component with current scroll data. If element is not 
+ * supplied, window scroll will be used.
+ * @param {(HTMLElement)} [element] - Element to track scrolling.
+ * @returns {ScrollData} Data for the current scroll state.
  */
-export const useScrollState = () => {
+export const useScrollState = (element?: ScrollElement) => {
     const [scroll, setScroll] = useState<ScrollData>(defaultScrollData())
-    useScrollAnimation(setScroll)
+    // useScrollAnimation(setScroll, element)
+    useScrollPosition(setScroll, element)
     return scroll
 }
 
 /**
  * Hook that calls a callback function when the window scroll state changes
- * @param effect - Callback
+ * If element is not supplied, window scroll will be used.
+ * @param {(ScrollData)=>void} effect - Callback
+ * @param {(HTMLElement)} [element] - Element to track scrolling.
  */
-export const useScrollPosition = (effect: ScrollCallback) => {
+export const useScrollPosition = (effect: ScrollCallback, element?: ScrollElement) => {
     const scrollData = useRef<ScrollData>(defaultScrollData())
 
     useEffect(() => {
         const handleScroll = () => {
-            const scroll = getScrollData(scrollData.current)
+            const scroll = getScrollData(scrollData.current, element)
             clearTimeout(scrollData.current.scrolling as number)
             scroll.scrolling = setTimeout(() => {
                 scrollData.current.scrolling = false
@@ -89,22 +115,24 @@ export const useScrollPosition = (effect: ScrollCallback) => {
             scrollData.current = scroll
             effect(scrollData.current)
         }
-        window.addEventListener('scroll', handleScroll, options)
-        return () => window.removeEventListener('scroll', handleScroll, options)
-    }, [effect])
+        const el = element ? element : window
+        el.addEventListener('scroll', handleScroll, options)
+        return () => el.removeEventListener('scroll', handleScroll, options)
+    }, [effect, element])
 }
 
 /**
- * Hook that calls a callback function when if scroll data has changed since 
- * the last animation frame
- * 
- * @param effect - Callback
+ * Hook that calls a callback function when scroll data has changed since 
+ * the last animation frame. If element is not supplied, window scroll will
+ * be used.
+ * @param {(ScrollData)=>void} effect - Callback
+ * @param {(HTMLElement)} [element] - Element to track scrolling.
  */
-export const useScrollAnimation = (effect: ScrollCallback) => {
+export const useScrollAnimation = (effect: ScrollCallback, element?: ScrollElement) => {
     const scrollData = useRef<ScrollData>(defaultScrollData())
 
     useAnimationFrame(() => {
-        const scroll = getScrollData(scrollData.current)
+        const scroll = getScrollData(scrollData.current, element)
         scrollData.current = scroll
         if (scrollData.current.scrolling) effect(scroll);
     })
